f71a39d63b948a8f4004862d11fff4d3e028a5f8
arifpehlivan <arifpehlivanapap@gmail.com>
2022-09-05 02:50:57 +0300

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import {AppBar, Button, Toolbar, Typography} from "@mui/material"
+import React, { useState } from 'react'
+import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from "@mui/material"
 import { Box } from '@mui/system'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+    const [value, setValue] = useState("");
     return (
-        <AppBar sx={{background: "linear-gradient(90deg, rgba(7,1,17,0.07076333951549374) 0%, rgba(25,12,12,0.8410714627647934) 0%)"}}>
+        <AppBar position='sticky' sx={{ background: "linear-gradient(90deg, rgba(7,1,17,0.07076333951549374) 0%, rgba(25,12,12,0.8410714627647934) 0%)" }}>
             <Toolbar>
                 <Typography variant='h4'>Blogs App</Typography>
+                <Box display="flex" marginLeft="auto" marginRight="auto">
+                    <Tabs textColor='inherit' value={value} onChange={(e, val) => setValue(val)}>
+                        <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+                        <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+                    </Tabs>
+                </Box>
                 <Box display="flex" marginLeft="auto">
-                    <Button variant='contained' sx={{margin: 1, borderRadius: 10}} color='warning'>Login</Button>
-                    <Button variant='contained' sx={{margin: 1, borderRadius: 10}} color='warning'>Signup</Button>
+                    <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+                    <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+                    <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
                 </Box>
             </Toolbar>
         </AppBar>
